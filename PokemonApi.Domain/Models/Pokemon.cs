6fc8486ddb91ca35b8d8ee128588d55967a8566d
SherifAshrf98 <sherifashrf6060@gmail.com>
2025-06-12 16:50:43 +0300

    adding project models

---
+++ PokemonApi.Domain/Models/Pokemon.cs
@@ -0,0 +1,13 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public ICollection<Review> Reviews { get; set; } = new HashSet<Review>();
+        public ICollection<PokemonOwner> PokemonOwners { get; set; } = new HashSet<PokemonOwner>();
+        public ICollection<PokemonCategory> PokemonCategories { get; set; } = new HashSet<PokemonCategory>();
+
+    }
+}
--- PokemonApi.Presentation/Program.cs
+++ PokemonApi.Presentation/Program.cs
@@ -8,9 +8,7 @@ namespace PokemonApi.Presentation
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
             var app = builder.Build();
